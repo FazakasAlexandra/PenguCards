0154ad21766f78777cd9c29515893c2f2532db26
user <user@Fazakas.local>
2024-05-27 09:30:48 +0300

    change config values

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -7,13 +7,13 @@ import {getFirestore, Firestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDlyt_QMASiihYn5B7LUtm-lHjkh0FwpAA',
-  authDomain: 'pengucards-349ea.firebaseapp.com',
-  projectId: 'pengucards-349ea',
-  storageBucket: 'pengucards-349ea.appspot.com',
-  messagingSenderId: '711971952885',
-  appId: '1:711971952885:web:190ceaa89cb8e89b450dbc',
-  measurementId: 'G-KKF2Y3B7YM',
+  apiKey: 'XXXX',
+  authDomain: 'XXXX',
+  projectId: 'XXXX',
+  storageBucket: 'XXXX',
+  messagingSenderId: 'XXXX',
+  appId: 'XXXX',
+  measurementId: 'XXXX',
 };
 
 // Initialize Firebase
